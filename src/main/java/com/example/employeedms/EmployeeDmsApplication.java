f83129e16014bfd7b1c3794d0844cf33c29e4d35
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-06-23 11:48:07 +0530

    employee-dms app

---
+++ src/main/java/com/example/employeedms/EmployeeDmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeedms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeedms/EmployeeDmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeedms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
